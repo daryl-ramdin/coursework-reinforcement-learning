827001018452a0495c7acb66ad0f2a0abf4a5bfb
Daryl Ramdin <daryl.ramdin.2@city.ac.uk>
2023-04-21 15:20:59 +0100

    Implemented SARSA

--- hiker.py
+++ hiker.py
@@ -17,11 +17,15 @@ class LearningBy:
 
 
 class HikerAgent:
-    def __init__(self,start_position, epsilon, alpha, gamma, policy_type, jungle: Jungle, learning_algorithm, seed=45):
+    def __init__(self,start_position, epsilon, alpha, gamma, policy_type, jungle: Jungle, sarsa=False, seed=45):
         self.environment = jungle
         self.initialise_q_matrix()
         self.current_position = start_position
-        self.learning_by = LearningBy(learning_algorithm)
+        #ref: https://www.geeksforgeeks.org/assign-function-to-a-variable-in-python/
+        if sarsa:
+            self.policy_update = self.sarsa
+        else:
+            self.policy_update = self.qlearn
         self.available_moves = self.environment.get_available_moves(self.current_position)
         self.initial_epsilon = epsilon
         self.epsilon = self.initial_epsilon
@@ -35,14 +39,14 @@ class HikerAgent:
         can_move = False
         #Let's see if we can move
         if len(self.available_moves) > 0:
-            next_action = self.get_next_action()
+            next_action = self.get_next_action(self.current_position,self.available_moves)
             #print("Next actions",next_action)
 
             #If there are no available actions, then do nothing
             if next_action is None:
                 can_move = False
             else:
-                #print("Moving through jungle with following action",best_action)
+                #Move
                 reward, new_position, available_moves, terminated, topography = self.environment.move(next_action)
 
                 #If new_position is none then it means we cannot move
@@ -51,8 +55,10 @@ class HikerAgent:
                 else:
                     #Update the cumulative reward
                     self.cumulative_reward+=reward
+
                     #The next step is to update the q matrix with the reward
-                    q_value_old, q_max, q_value_new = self.update_q_matrix(reward,self.current_position, new_position, next_action)
+                    self.policy_update(reward,self.current_position, new_position, next_action,available_moves)
+
                     #Update the agent's state and available moves
                     self.current_position = new_position
                     self.available_moves = available_moves
@@ -70,27 +76,55 @@ class HikerAgent:
     def get_q_value(self,state,action_index):
         return self.q_matrix[self.environment.get_r_index((state[0],state[1])), action_index]
 
-    def update_q_matrix(self, reward, state, new_state, action):
+    def qlearn(self, reward, state, next_state, action, available_moves):
         #We will update the Q value for the given state and action
+        #For Q Learning we do not use the available moves
+
         #The state defines the position at which the action was taken
         #Get the index into the Q matrix
         shape = self.environment.reward_matrix.shape
         row, col = state[0],state[1]
         action_index = self.environment.all_actions[action]
+
         #Get the old q_value which is found in the q matrix for state,action
         q_value_old = self.get_q_value(state,action_index)
+
         #For the new_state, get the action that has the best estimated q value for the new_state
-        new_state_index = self.environment.get_r_index((new_state[0],new_state[1]))
+        new_state_index = self.environment.get_r_index((next_state[0],next_state[1]))
         q_max = max(self.q_matrix[new_state_index])
+
         #Update the old q value
-        q_value_new = q_value_old + self.alpha * ( reward+(self.gamma*q_max) - q_value_old )
+        q_value_new = q_value_old + self.alpha * ( reward+(self.gamma*q_max) - q_value_old)
 
         self.q_matrix[self.environment.get_r_index((row,col)), action_index] = q_value_new
         return q_value_old, q_max, q_value_new
 
-    def get_next_action(self):
+    def sarsa(self, reward, state, next_state, action, available_moves):
+        # ref: https://towardsdatascience.com/q-learning-and-sasar-with-python-3775f86bd178
+        # We will update the Q value for the given state and action
+        # The state defines the position at which the action was taken
+        # Get the index into the Q matrix
+        shape = self.environment.reward_matrix.shape
+        row, col = state[0], state[1]
+        action_index = self.environment.all_actions[action]
+
+        # Get the old q_value which is found in the q matrix for state,action
+        q_value_old = self.get_q_value(state, action_index)
+
+        #For SARSA, we get the Q value for taking an action in the next state
+        next_state_action = self.get_next_action(next_state,available_moves)
+        next_state_action_index = self.environment.all_actions[next_state_action]
+        q_value_next = self.get_q_value(next_state, next_state_action_index)
+
+        # Update the old q value
+        q_value_new = q_value_old + self.alpha * (reward + (self.gamma * q_value_next) - q_value_old)
+
+        self.q_matrix[self.environment.get_r_index((row, col)), action_index] = q_value_new
+        return q_value_old, q_value_next, q_value_new
+
+    def get_next_action(self,position, available_moves):
         #Create a list of available moves from the curent state
-        avlbl_moves = np.array([x for x in self.available_moves.values()])
+        avlbl_moves = np.array([x for x in available_moves.values()])
 
         next_action = None
         #If there are no available moves then the best action is none
@@ -98,13 +132,16 @@ class HikerAgent:
             next_action = None
         else:
             #Look into the q matrix and get the action from the current state with the largest q values
-            r_index = self.environment.get_r_index(self.current_position)
+            r_index = self.environment.get_r_index(position)
+
             # Let's select the move with the highest expected q_max
             q_max = self.q_matrix[r_index, avlbl_moves].max()
+
             # Let's get all the available actions that have this q_max. Important to note
             # that flatnonzero will return more indices into moves
             best_action_values = avlbl_moves[np.flatnonzero(self.q_matrix[r_index, avlbl_moves] == q_max)]
             best_actions = [i[0] for i in list(self.environment.all_actions.items()) if i[1] in best_action_values]
+
             #Let's look at our learning policy
             if self.policy_type == 'Greedy':
                 #Randomly choose best action. If there is only one, the code below will select it
@@ -116,12 +153,10 @@ class HikerAgent:
                 if np.random.uniform() > self.epsilon:
                     #We exploit. Choose the available move that has the highest estimated reward
                     next_action = np.random.choice(best_actions)
-                    # print("Selecting greedy action")
                 else:
                     # We explore. Randomly choose from the available moves
                     action_value = np.random.choice(avlbl_moves)
                     next_action = [i[0] for i in list(self.environment.all_actions.items()) if i[1] == action_value][0]
-                    # print("Selecting random action")
 
         return next_action
 
@@ -146,6 +181,8 @@ class HikerAgent:
             topography = self.environment.get_topography(position)
             print("Move to:",position,topography)
             if topography in ["S","E"]: break
+
+
 
 
 
